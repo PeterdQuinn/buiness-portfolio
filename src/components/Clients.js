698c310c1ba05273bb0dbf81f3da1a012d6db291
Peter Quinn <peter@quickorganics.com>
2023-11-27 09:33:49 -0700

    updates

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -2,54 +2,54 @@
 
 const PastClients = () => {
   return (
-    <section id="past-clients" className="container mx-auto p-4">
-      <h2 className="text-2xl font-bold text-center mb-8">Clients</h2>
+    <section id="past-clients" className="container mx-auto p-4 bg-light-gray-200 rounded-lg">
+      <h2 className="text-3xl font-bold text-center mb-8 text-dark-blue-500">Clients</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         
         {/* Tri-Tek Electronics */}
-        <div className="client-card p-4 border border-gray-200 rounded shadow-sm hover:shadow-md">
+        <div className="client-card p-4 bg-client-color rounded-lg shadow-lg hover:shadow-xl transition-shadow">
           <h3 className="text-lg font-semibold mb-2">Tri-Tek Electronics</h3>
           <p className="text-sm mb-3">Enhanced the e-commerce website with SEO optimization and design updates, improving user experience and online visibility.</p>
           <a href="https://www.tritekelectronics.com/" target="_blank" rel="noopener noreferrer" className="client-link">Visit Site</a>
         </div>
 
         {/* Junior's Custom Towing LLC */}
-        <div className="client-card p-4 border border-gray-200 rounded shadow-sm hover:shadow-md">
+        <div className="client-card p-4 bg-client-color rounded-lg shadow-lg hover:shadow-xl transition-shadow">
           <h3 className="text-lg font-semibold mb-2">Junior's Custom Towing LLC</h3>
           <p className="text-sm mb-3">Developed and managed the website for this family-owned towing company, enhancing online visibility and customer engagement.</p>
           <a href="https://www.juniorscustomtowing.com/" target="_blank" rel="noopener noreferrer" className="client-link">Visit Site</a>
         </div>
 
         {/* Nomadic Cooling */}
-        <div className="client-card p-4 border border-gray-200 rounded shadow-sm hover:shadow-md">
+        <div className="client-card p-4 bg-client-color rounded-lg shadow-lg hover:shadow-xl transition-shadow">
           <h3 className="text-lg font-semibold mb-2">Nomadic Cooling</h3>
           <p className="text-sm mb-3">Collaborated on digital footprint enhancement through SEO and web development, focusing on product visibility and performance.</p>
           <a href="https://www.nomadiccooling.com/" target="_blank" rel="noopener noreferrer" className="client-link">Visit Site</a>
         </div>
 
         {/* Full Stack Services LLC */}
-        <div className="client-card p-4 border border-gray-200 rounded shadow-sm hover:shadow-md">
+        <div className="client-card p-4 bg-client-color rounded-lg shadow-lg hover:shadow-xl transition-shadow">
           <h3 className="text-lg font-semibold mb-2">Full Stack Services LLC</h3>
           <p className="text-sm mb-3">Founded and developed Full Stack Services LLC, an SEO company specializing in comprehensive digital marketing strategies and web development services.</p>
           <a href="https://fullstackservicesllc.net/" target="_blank" rel="noopener noreferrer" className="client-link">Visit Site</a>
         </div>
 
         {/* Full Stack SEO */}
-        <div className="client-card p-4 border border-gray-200 rounded shadow-sm hover:shadow-md">
+        <div className="client-card p-4 bg-client-color rounded-lg shadow-lg hover:shadow-xl transition-shadow">
           <h3 className="text-lg font-semibold mb-2">Full Stack SEO</h3>
           <p className="text-sm mb-3">Created a demo website demonstrating expertise in SEO strategies and web design.</p>
           <a href="https://www.fullstackseo.net/" target="_blank" rel="noopener noreferrer" className="client-link">Visit Site</a>
         </div>
 
         {/* Crypto DApps */}
-        <div className="client-card p-4 border border-gray-200 rounded shadow-sm hover:shadow-md">
+        <div className="client-card p-4 bg-client-color rounded-lg shadow-lg hover:shadow-xl transition-shadow">
           <h3 className="text-lg font-semibold mb-2">Crypto DApps</h3>
           <p className="text-sm mb-3">Developed decentralized applications showcasing blockchain technology and smart contract development skills.</p>
           <a href="https://dapps-rouge.vercel.app/" target="_blank" rel="noopener noreferrer" className="client-link">Visit Site</a>
         </div>
 
         {/* Chillsbury Doge Boy */}
-        <div className="client-card p-4 border border-gray-200 rounded shadow-sm hover:shadow-md">
+        <div className="client-card p-4 bg-client-color rounded-lg shadow-lg hover:shadow-xl transition-shadow">
           <h3 className="text-lg font-semibold mb-2">Chillsbury Doge Boy</h3>
           <p className="text-sm mb-3">Developed an engaging NFT project platform for Chillsbury Doge Boy, focusing on blockchain integration and a user-friendly interface to facilitate NFT transactions and interactions.</p>
           <a href="https://www.chillsburydogeboy.com/" target="_blank" rel="noopener noreferrer" className="client-link">Visit Project</a>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,23 @@
-// components/Header.js
-
+import { useEffect, useState } from 'react';
+import layout from '../components//Layout';
 const Header = () => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+    document.documentElement.classList.toggle('dark', newTheme === 'dark');
+  };
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      setTheme(savedTheme);
+      document.documentElement.classList.toggle('dark', savedTheme === 'dark');
+    }
+  }, []);
+
   return (
     <header className="bg-gray-800 text-white p-4 shadow-md">
       <nav className="container mx-auto flex justify-between items-center">
@@ -10,6 +27,13 @@ const Header = () => {
           <a href="#services" className="hover:text-blue-500 transition-colors duration-300">Services</a>
           <a href="#past-clients" className="hover:text-blue-500 transition-colors duration-300">Clients</a>
         </div>
+        {/* Theme Toggle Button */}
+        <button
+          onClick={toggleTheme}
+          className="text-white hover:text-blue-500 transition-colors duration-300"
+        >
+          Toggle Theme
+        </button>
       </nav>
     </header>
   );
